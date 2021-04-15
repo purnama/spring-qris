02bbbf009e4f0c463a63f7fe2e970a4cb4ce8b83
Arthur Purnama <arthur.purnama@astrapay.com>
2021-04-15 22:11:38 +0700

    prepare for release to maven central add validator for mandatory field bug fixes

--- src/main/java/id/purnama/qris/validation/PostalCodeValidator.java
+++ src/main/java/id/purnama/qris/validation/PostalCodeValidator.java
@@ -6,8 +6,6 @@ import id.purnama.qris.validation.constraints.PostalCode;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * <b>4.7.15</b> Postal Code (ID "61")<br/>
@@ -17,19 +15,10 @@ public class PostalCodeValidator implements ConstraintValidator<PostalCode, Map<
 
     @Override
     public boolean isValid(Map<Integer, QrisDataObject> value, ConstraintValidatorContext context) {
-        if("ID".equals(value.get(58).getValue())){
-            if(value.get(61) != null){
-                return isNumber(value.get(61).getValue());
-            }
-            return false;
+        if ("ID".equals(value.get(58).getValue())) {
+            return value.get(61) != null;
         }
         return true;
     }
 
-    private boolean isNumber(String str){
-        String regex = "^[0-9]{5}$";
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(str);
-        return m.matches();
-    }
 }
